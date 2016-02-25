40acf9a83453451c87158e069d69298a98b69751
Ali Moghnieh <SupaHam.me@gmail.com>
2016-02-25 23:56:13 +0000

    Forgot to commit MutableExtent class >.>

---
+++ src/main/java/com/blurengine/blur/modules/extents/MutableExtent.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2016 Ali Moghnieh
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.blurengine.blur.modules.extents;
+
+/**
+ * Represents a mutable {@link Extent}. Although an empty interface, all mutable access will be provided per {@link Extent} implementation. This class
+ * serves as no more than a purpose of identifying mutable extents.
+ */
+public interface MutableExtent extends Extent {
+}
