5c85288c985d52dec15cb2ecf1ec8a62673115d3
Ali Moghnieh <supaham@supaham.com>
2017-07-29 16:58:40 +0100

    Implement initial TextFormatter for parameters in messages.

---
+++ src/main/java/com/blurengine/blur/text/TextFormatter.kt
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2017 Ali Moghnieh
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.blurengine.blur.text
+
+import net.kyori.text.Component
+import net.kyori.text.KeybindComponent
+import net.kyori.text.ScoreComponent
+import net.kyori.text.SelectorComponent
+import net.kyori.text.TextComponent
+import net.kyori.text.TranslatableComponent
+import net.kyori.text.event.ClickEvent
+import net.kyori.text.event.HoverEvent
+
+object TextFormatter {
+    val PATTERN = """(?<!\\)\{(\d+)}""".toPattern()
+
+    fun format(component: Component, args: Array<Any?>): Component {
+        if (args.isEmpty()) return component
+        val componentArgs: Map<Int, Component> = args
+                .mapIndexed { idx, it -> idx to it }
+                .filter { it.second is Component }
+                .map { it.first to it.second as Component }.toMap()
+
+        // TODO update when extract-buildable is on master
+        val builder: Component.Builder<*, *> = when (component) {
+            is KeybindComponent -> KeybindComponent.builder().apply {
+                keybind(replaceParams(component.keybind(), args))
+            }
+            is ScoreComponent -> ScoreComponent.builder().apply {
+                name(replaceParams(component.name(), args))
+                objective(replaceParams(component.objective(), args))
+                component.value()?.apply { value(replaceParams(this, args)) }
+            }
+            is SelectorComponent -> SelectorComponent.builder().apply {
+                pattern(replaceParams(component.pattern(), args))
+            }
+            is TextComponent -> TextComponent.builder().apply {
+                val matcher = PATTERN.matcher(component.content())
+                val sb = StringBuffer()
+                var appendedComponent = false
+                while(matcher.find()) {
+                    val idx = matcher.group(1).toInt()
+                    if (idx in componentArgs) {
+                        appendedComponent = true
+                        append(format(componentArgs[idx]!!, args))
+                        matcher.appendReplacement(sb, "")
+                    } else {
+                        val str = if (idx > args.lastIndex) "null" else args[idx].toString()
+                        matcher.appendReplacement(sb, str)
+                    }
+                }
+                if (appendedComponent) {
+                    content(sb.toString())
+                    val tailSb = matcher.appendTail(StringBuffer())
+                    append(TextComponent.of(tailSb.toString()))
+                } else {
+                    matcher.appendTail(sb)
+                    content(sb.toString())
+                }
+            }
+            is TranslatableComponent -> TranslatableComponent.builder().apply {
+                key(replaceParams(component.key(), args))
+                args(component.args().map { format(it, args) })
+            }
+            else -> throw UnsupportedOperationException("Unknown type ${component.javaClass.canonicalName}")
+        }
+        builder.mergeColor(component)
+        builder.mergeDecorations(component)
+        
+        component.hoverEvent()?.apply {
+            builder.hoverEvent(HoverEvent(action(), format(value(), args)))
+        }
+        
+        component.clickEvent()?.apply {
+            builder.clickEvent(ClickEvent(action(), replaceParams(value(), args)))
+        }
+        
+        
+        for (child in component.children()) {
+            builder.append(format(child, args))
+        }
+        return builder.build()
+    }
+
+    fun replaceParams(string: CharSequence, args: Array<Any?>): String {
+        val matcher = PATTERN.matcher(string)
+        val sb = StringBuffer()
+        while(matcher.find()) {
+            val idx = matcher.group(1).toInt()
+            val str = if (idx > args.lastIndex) "null" else args[idx].toString()
+            matcher.appendReplacement(sb, str)
+        }
+        matcher.appendTail(sb)
+        return sb.toString()
+    }
+}
---
+++ src/test/java/com/blurengine/blur/text/TextFormatterKotlin.kt
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2017 Ali Moghnieh
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.blurengine.blur.text
+
+import net.kyori.text.Component
+import net.kyori.text.KeybindComponent
+import net.kyori.text.ScoreComponent
+import net.kyori.text.SelectorComponent
+import net.kyori.text.TextComponent
+import net.kyori.text.TranslatableComponent
+import net.kyori.text.event.ClickEvent
+import net.kyori.text.event.HoverEvent
+import net.kyori.text.format.TextColor
+import net.kyori.text.format.TextDecoration
+import org.junit.Assert
+import org.junit.Test
+
+class TextFormatterKotlin {
+    @Test
+    fun testBasicEmptyArgs() {
+        val baseActual = TextComponent.of("foo")
+        val actual: Component = TextFormatter.format(baseActual, arrayOf())
+        val expected = TextComponent.of("foo")
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testComplexEmptyArgs() {
+        val baseActual = TextComponent.of("foo ").append(TextComponent.of("bar"))
+        val actual: Component = TextFormatter.format(baseActual, arrayOf())
+        val expected = TextComponent.of("foo ").append(TextComponent.of("bar"))
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testStringArg() {
+        val arg = "bar"
+        val baseActual = TextComponent.of("foo {0}")
+        val actual: Component = TextFormatter.format(baseActual, arrayOf(arg))
+        val expected = TextComponent.of("foo $arg")
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testComponentArg() {
+        val arg = TextComponent.of("bar")
+        val baseActual = TextComponent.of("foo {0}")
+        val actual: Component = TextFormatter.format(baseActual, arrayOf(arg))
+        val expected = TextComponent.of("foo ").append(arg)
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testMoreComplexComponentArg() {
+        val arg = TextComponent.of("bar")
+        val baseActual = TextComponent.of("foo {0} baz")
+        val actual: Component = TextFormatter.format(baseActual, arrayOf(arg))
+        val expected = TextComponent.of("foo ").append(arg).append(TextComponent.of(" baz"))
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testKeybindStringArg() {
+        val arg = "bar"
+        val baseActual = KeybindComponent.of("foo {0}")
+        val actual: Component = TextFormatter.format(baseActual, arrayOf(arg))
+        val expected = KeybindComponent.of("foo bar")
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testScoreStringArg() {
+        val arg = "bar"
+        val arg2 = "baz"
+        val arg3 = "foo"
+        val baseActual = ScoreComponent.of("foo {0}", "bar {1}", "{2} baz")
+        val actual: Component = TextFormatter.format(baseActual, arrayOf(arg, arg2, arg3))
+        val expected = ScoreComponent.of("foo $arg", "bar $arg2", "$arg3 baz")
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testSelectorStringArg() {
+        val arg = "bar"
+        val baseActual = SelectorComponent.of("foo {0}")
+        val actual: Component = TextFormatter.format(baseActual, arrayOf(arg))
+        val expected = SelectorComponent.of("foo bar")
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testTranslatableStringArg() {
+        val arg = "bar"
+        val arg2 = "baz"
+        val baseActual = TranslatableComponent.of("foo {0}", listOf(TextComponent.of("foo {1}")))
+        val actual: Component = TextFormatter.format(baseActual, arrayOf(arg, arg2))
+        val expected = TranslatableComponent.of("foo $arg", listOf(TextComponent.of("foo $arg2")))
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testStyles() {
+        val arg = "bar"
+        val baseActual = TextComponent.of("foo {0}")
+                .color(TextColor.RED)
+                .decoration(TextDecoration.BOLD, true)
+                .clickEvent(ClickEvent(ClickEvent.Action.RUN_COMMAND, "/run {0}"))
+                .hoverEvent(HoverEvent(HoverEvent.Action.SHOW_TEXT, TextComponent.of("/run {0}")))
+        val actual = TextFormatter.format(baseActual, arrayOf(arg))
+        val expected = TextComponent.of("foo $arg")
+                .color(TextColor.RED)
+                .decoration(TextDecoration.BOLD, true)
+                .clickEvent(ClickEvent(ClickEvent.Action.RUN_COMMAND, "/run $arg"))
+                .hoverEvent(HoverEvent(HoverEvent.Action.SHOW_TEXT, TextComponent.of("/run $arg")))
+        Assert.assertEquals(expected, actual)
+    }
+}
