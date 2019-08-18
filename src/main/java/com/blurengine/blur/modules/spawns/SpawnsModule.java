1b4333995ccd6598432dacc51642a2c6373fa1d4
Xorgon <norman.reggox@gmail.com>
2019-08-18 20:39:22 +0100

    Replaced wildcard import with single class imports.

--- src/main/java/com/blurengine/blur/modules/spawns/SpawnsModule.java
+++ src/main/java/com/blurengine/blur/modules/spawns/SpawnsModule.java
@@ -48,7 +48,11 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.PlayerRespawnEvent;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 
 import javax.annotation.Nonnull;
